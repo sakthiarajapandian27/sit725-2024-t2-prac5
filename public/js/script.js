0bb6606b53ff4f6c6a7b176b09cea9f9e81f6198
sakthiarajapandian27 <s224789408@deakin.edu.au>
2024-08-08 23:38:08 +1000

    push changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,60 @@
-
-
-const clickMe = () => {
-    $.ajax({url: "http://localhost:3040/addTwoNumber?n1=12&n2=12", success: function(result){
-     alert(result?.data)
-      }});
-    // alert("Thanks for clicking me. Hope you have a nice day!")
+const formSubmitted = () => {
+    let formData = {};
+    formData.firstName = $('#firstName').val();
+    formData.lastName = $('#lastName').val();
+    formData.emailId = $('#emailId').val();
+    formData.phoneNumber = $('#phoneNumber').val();
+    formData.query = $('#query').val();
+    console.log(formData, 'formData');
+    postQuery(formData);
 }
-$(document).ready(function () {
- 
-    $('.materialboxed').materialbox();
-    $('#clickMeButton').click(() => {
-        clickMe();
-    })
+
+function postQuery(queryData){
+    $.ajax({
+        url:'/api/query',
+        type:'POST',
+        data:queryData,
+        success: (result)=>{
+          console.log(result,"result")
+            if (result.statusCode === 201) {
+                alert('cat post successful');
+            }
+        }
+    });
+}
+let addCards;
+console.log(addCards,"addCards")
+function getAllQuery(){
+    $.get('/api/queries', (response)=>{
+        // response's data is in array format, so we can use it
+        console.log(response,"response")
+        addCards = response;
+        console.log(addCards,"response1")
+        if (response.statusCode === 200) {
+            addCards= response;
+        }
+
+      var text = addCards.at(-1);
+      console.log(text,"text")
+      firstNameText = `First name: ${text.firstName}`;
+      document.getElementById('firstName').innerHTML = firstNameText;
+      lastNameText = `Last name: ${text.lastName}`;
+      document.getElementById('lastName').innerHTML = lastNameText;
+      emailId = `Email Id: ${text.emailId}`;
+      document.getElementById('email').innerHTML = emailId;
+      phoneNumber = `Phone number: ${text.phoneNumber}`;
+      document.getElementById('phone').innerHTML = phoneNumber;
+      query = `Query: ${text.query}`;
+      document.getElementById('query').innerHTML = query;
+    });
+}
+
+
+$(document).ready(function(){
+    
+    $('#formSubmit').click(()=>{
+        console.log("button click");
+        formSubmitted();
+    });
+    getAllQuery();
 });
--- server_get.js
+++ server_get.js
@@ -1,25 +1,51 @@
-const express= require("express");
-const app= express();
-app.use(express.static('public'))
+const express = require('express');
+const mongoose = require("mongoose");
+let app = express();
+const cors = require('cors');
 
-const addTwoNumber= (n1,n2) => {
-    return n1+n2;
-}
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+  };
+ 
+app.use(cors(corsOptions));
 
-app.get("/addTwoNumber", (req,res)=>{
-    const n1= parseInt(req.query.n1);
-    const n2=parseInt(req.query.n2);
-    const result = addTwoNumber(n1,n2);
-    res.json({statuscocde:200, data: result }); 
+mongoose.connect('mongodb+srv://sakthiapandian:ONdupx1TS8FpGHco@clustercar.9zvyi.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true})
+const userSchema = new mongoose.Schema({
+    firstName : String,
+   lastName : String,
+    emailId : String,
+    phoneNumber : String,
+    query: String,
+})
+const User = mongoose.model('User', userSchema)
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.post('/api/query', async(req,res)=>{
+    const { firstName,lastName, emailId, phoneNumber, query } = req.body;
+    const user = new User({firstName,lastName, emailId, phoneNumber, query});
+    const savedUser = await user.save()
+    res.json({message:'data inserted successfully', savedUser})
+})
+
+app.get('/', function (req,res) {
+  res.render('indexMongo.html');
 });
 
-app.get("/Display", (req, res) => {
-    const n1 = "<html><body><H1>HELLO THERE </H1></body></html>";
-    res.set('Content-Type', 'text/html');
-    res.send(Buffer.from(n1));     
-})
-console.log (addTwoNumber(19,12));
-const port=3040;
-app.listen(port,()=> {
-    console.log("hello i'm listening to port "+port);
-})
+app.get('/api/queries', async(req,res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+ }catch(err){
+  return res.status(500).json({message:'error retrived in getting users'})
+    }
+
+});
+
+let port = process.env.port || 3000;
+app.listen(port, ()=>{
+    console.log('express server started');
+});
